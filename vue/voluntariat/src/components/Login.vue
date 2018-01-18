b289ecb3146de87331e5c24f41568b8d79896272
GreyGonz <gerardrey@iesebre.com>
2018-01-18 21:22:59 +0100

    Sync

---
+++ vue/voluntariat/src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="message">
+        <h1>{{ msg }}</h1>
+        <button @click="change()">Change</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Login'
+    }
+  },
+  methods: {
+    change () {
+      this.msg = 'Changed'
+    }
+  }
+}
+</script>
